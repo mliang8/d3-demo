8baa58a5dbfe9a2f85413f5b82f196c55aff542e
mliang8 <Amber@Mengyus-MacBook-Air.local>
2017-03-14 07:48:46 -0500

    Module 7 assignment completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Mengyu Liang
-//Module 7 D3 Fundation
+//Module 7 D3 Fundation- bubble chart 
 
 //execute the following script when window is loaded
 window.onload=function(){
@@ -24,7 +24,7 @@ window.onload=function(){
 		.attr("x",50)
 		.attr("y",50)
 		.style("fill","#ffeb66");
-
+	//create an array to include the four cities and their population as data
 	var cityPop=[
 		{
 			city: "Kunming",
@@ -43,17 +43,19 @@ window.onload=function(){
 			population: 4580000
 		}
 	];
-	
+	//using continuous linear scale function to evenly distribute the bubbles in x-direction
 	var x=d3.scaleLinear()
 		.range([90,705])
 		.domain([0,3]);
-
+	//find the min value of the data
 	var minPop=d3.min(cityPop,function(d){
 		return d.population;
 	});
+	//find the max value of the data
 	var maxPop=d3.max(cityPop,function(d){
 		return d.population;
 	});
+	//create a continuous color scheme to add fill colors to the buubles proportionally
 	var color = d3.scaleLinear()
         .range([
             "#eff3ff",
@@ -63,12 +65,12 @@ window.onload=function(){
             minPop, 
             maxPop
         ]);
+    //using continuous linear function to scale the horizontal location of bubbles based on population size
 	var y=d3.scaleLinear()
 		.range([450,50])
 		.domain([0,4500000
 		]);
-
-
+	//create circles based on population size
 	var circles=container.selectAll(".circles") //create an empty selction for adding circles into 
 		.data(cityPop) //feed in the array to draw the circles
 		.enter() //join the data to the slection
@@ -93,14 +95,14 @@ window.onload=function(){
         .style("stroke", function(d, i){ //add a fill based on the color scale generator
             return color(d.population);
         }); //black circle stroke
-
+    //create a y-axis to the left of the scaled bubbles
     var yAxis=d3.axisLeft(y);
-
+    //create a group class to store the axis on the y-axis including the increments
     var axis=container.append("g")
     	.attr("class","axis")
     	.attr("transform","translate(50,0)")
     	.call(yAxis);
-
+    //create a title for the graphic and center-align it, and furthur define its location 
     var title=container.append("text")
     	.attr("class","title")
     	.attr("text-anchor","middle")
@@ -112,12 +114,13 @@ window.onload=function(){
         .data(cityPop)
         .enter()
         .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
+        .attr("class", "labels") //define a class for the labels
+        .attr("text-anchor", "left") //align the labels to the left
         /*.attr("x", function(d,i){
             //horizontal position to the right of each circle
             return x(i) + Math.sqrt(d.population * 0.0001 / Math.PI)+30;
         })*/
+		//define the horizontal position 
         .attr("y", function(d){
             //vertical position centered on each circle
             return y(d.population)+45;
@@ -125,6 +128,7 @@ window.onload=function(){
         /*.text(function(d){
             return d.city + ", Pop. " + d.population;
         });*/
+	//store the city nams of the data into a separate class and define its location
 	var nameLine=labels.append("tspan")
 		.attr("class","nameLine")
 		.attr("x",function(d,i){
@@ -135,7 +139,7 @@ window.onload=function(){
 		})
 	//create format generator
 	var format=d3.format(",");
-	//create the second line of labels
+	//create the second line of labels which includeds the population information 
 	var popLine=labels.append("tspan")
 		.attr("class","popLine")
 		.attr("x",function(d,i){
@@ -145,10 +149,5 @@ window.onload=function(){
 		.text(function(d){
 			return "Pop. "+format(d.population);
 		});
-
-
-
-
-
 	//console.log(circles);
 };
